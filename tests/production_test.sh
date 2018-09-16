06d5df3d0d0f0d5e37007da355f32562c7de3c3b
dasun <dasun@sprii.com>
2018-09-16 21:43:07 +0530

    fix

--- tests/production_test.sh
+++ tests/production_test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # integration-tests.sh
 echo "Starting integration tests..."
-res1=$(curl -s -o /dev/null -w "%{http_code}" http://$1/homersimpson)
+res1=$(curl -s -o /dev/null -w "%{http_code}" http://$1/homersimpson/)
 
 if [ "$res1" != "200" ]; then
  echo "Path /homersimpson test failed. Aborting..."
  exit 1
 fi
 
-res2=$(curl -s -o /dev/null -w "%{http_code}" http://$1/covilha)
+res2=$(curl -s -o /dev/null -w "%{http_code}" http://$1/covilha/)
 if [ "$res2" != "200" ]; then
  echo "Path /covilha test failed. Aborting..."
  exit 1
